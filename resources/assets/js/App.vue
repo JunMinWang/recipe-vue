7aa666adfeeb51fa7063ccb6e544c87c5a70e4cd
王俊閔 <jimmy.wang@program.com.tw>
2017-12-27 14:30:30 +0800

    初始化Vue元件

---
+++ resources/assets/js/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <div class="navbar">
+            <div class="navbar__brand">
+                <router-link to="/">Recipe</router-link>
+            </div>
+            <ul class="navbar__list">
+                <li class="navbar__item">
+                    <router-link to="/login">Login</router-link>
+                </li>
+                <li class="navbar__item">
+                    <router-link to="/register">Register</router-link>
+                </li>
+                <li class="navbar__item">
+                    <router-link to="/logout">Logout</router-link>
+                </li>
+            </ul>
+        </div>
+        
+        <router-view></router-view>
+    </div>
+</template>
